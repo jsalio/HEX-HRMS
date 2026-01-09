ef99cceecdce5ae9192549d2d8fda137780c10f1
Jorge Rodriguez <jrodriguez@novosit.com>
2026-01-09 14:09:24 -0400

    move code

--- infra/hrms-web/src/app/app.routes.ts
+++ infra/hrms-web/src/app/app.routes.ts
@@ -3,11 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
+    loadComponent: () => import('./ui/login/login.component').then(m => m.LoginComponent)
   },
   {
     path: 'create-account',
-    loadComponent: () => import('./create-account/create-account.component').then(m => m.CreateAccountComponent)
+    loadComponent: () => import('./ui/create-account/create-account.component').then(m => m.CreateAccountComponent)
   },
   {
     path: '',
